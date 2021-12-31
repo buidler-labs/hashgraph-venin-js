d20a4499aa9630471138b6f42af0af195a9d8ffc
Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
2021-12-31 10:00:10 +0200

    Exported both the LiveJson and the LiveContract from the library to have typedoc pick it up.

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export * from './lib/ApiSession';
 export * from './lib/HederaNetwork';
+export * from './lib/live/LiveContract';
+export * from './lib/live/LiveJson';
 export * from './lib/static/Contract';
 export * from './lib/static/Json';
