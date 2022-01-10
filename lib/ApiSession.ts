68cd21c2740bc35d34d94b39d8fa68dd09ba906c
Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
2022-01-10 17:30:02 +0200

    Alphabetically aranged method names in ApiSession.

--- lib/ApiSession.ts
+++ lib/ApiSession.ts
@@ -61,58 +61,52 @@ export class ApiSession implements SolidityAddressable {
   }
 
   /**
+   * Executes a {@link TransactionReceiptQuery} with the purpose of optaining a {@link TransactionReceipt} for apreviously executed, network, operation.
+   */
+   public async execute(transaction: TransactionReceiptQuery): Promise<TransactionReceipt>;
+   /**
+    * Queries/Executes a contract function, returning the {@link ContractFunctionResult} if successfull.
+    */
+   public async execute(transaction: ContractFunctionCall): Promise<ContractFunctionResult>;
+   /**
+    * A catch-all/generic {@link Transaction} execution operation yeilding, upon success, of a {@link TransactionResponse}.
+    */
+   public async execute(transaction: Transaction): Promise<TransactionResponse>;
+   public async execute(transaction: ContractFunctionCall|TransactionReceiptQuery|Transaction): Promise<ContractFunctionResult|TransactionReceipt|TransactionResponse> {
+     const isContractTransaction = transaction instanceof ContractCallQuery || transaction instanceof ContractExecuteTransaction;
+     const isReceiptQuery = transaction instanceof TransactionReceiptQuery;
+     const txResponse = await transaction.execute(this.client);
+ 
+     if (isContractTransaction) {
+       if (txResponse instanceof TransactionResponse) {
+         const txRecord = await txResponse.getRecord(this.client);
+   
+         // Inspired from https://github.com/hashgraph/hedera-sdk-js/blob/c4a8d339648651a545782089ae4b18b972f2e356/src/transaction/TransactionResponse.js#L39
+         // since, at this step, getRecord errors are the same as getReceipt ones
+         if (txRecord.receipt.status !== Status.Success) {
+           throw new ReceiptStatusError({
+               transactionReceipt: txRecord.receipt,
+               status: txRecord.receipt.status,
+               transactionId: txResponse.transactionId,
+           });
+         }
+         return txRecord.contractFunctionResult;
+       } else {
+         // Constant function call (query) was done
+         return txResponse as ContractFunctionResult;
+       }
+     } else if (isReceiptQuery) {
+       return (txResponse as unknown) as TransactionReceipt;
+     }
+     // Reaching this point means that the executed transaction is generic in which case we just return it rawly
+     return txResponse as TransactionResponse;
+   }
+
+  /**
    * Retrieves the solidity-address of the underlying {@link AccountId} that's operating this session.
    */
   async getSolidityAddress(): Promise<string> {
     return this.accountId.toSolidityAddress();
-  }
-
-  /**
-   * Given an {@link UploadableEntity}, it triest ot upload it using the currently configured {@link ApiSession} passing in-it any provided {@link args}.
-   * 
-   * @param {Uploadable} what - The {@link UploadableEntity} to push through this {@link ApiSession}
-   * @param {*} args - A list of arguments to pass through the upload operation itself.
-   *                   Note: this list has, by convention, at various unpaking stages in the call hierarchy, the capabilities to specify SDK behaviour through
-   *                         eg. "_file" ({@link UploadableEntity}) or "_contract" ({@link Contract})
-   * @returns - An instance of the {@link UploadableEntity} concrete result-type which is a subtype of {@link LiveEntity}.
-   */
-  public async upload<T extends LiveEntity<R>, R>(what: UploadableEntity<T, R>, ...args: any[]): Promise<T>;
-
-  /**
-   * Given a raw JSON {@link object}, it triest ot upload it using the currently configured {@link Client} passing in-it any provided {@link args}.
-   * 
-   * `Note:` This is the same as calling the more verbose equivalent of `upload(new Json(what))`.
-   * 
-   * Example of usage:
-   * ```js
-   * await apiSession.upload({a: 1, {b: "c"}})
-   * ```
-   * 
-   * @param {object} what - The {@link Json}-acceptable payload to push through this {@link ApiSession}
-   * @param {*} args - A list of arguments to pass through the upload operation itself.
-   *                   Note: this list has, by convention, at various unpaking stages in the call hierarchy, the capabilities to specify SDK behaviour through
-   *                         eg. "_file" ({@link Uploadable}) or "_contract" ({@link Contract})
-   * @returns - An instance of the associated {@link LiveJson} resulting {@link LiveEntity}.
-   */
-  public async upload(what: object, ...args: any[]): Promise<LiveJson>;
-
-  // Overload implementation
-  public async upload<T extends LiveEntity<R>, R>(what: UploadableEntity<T, R>|object, ...args: any[]): Promise<T|LiveJson> {
-    let uploadableWhat: UploadableEntity<T, R>;
-
-    if (what instanceof UploadableEntity === false) {
-      // Try to go with a live-json upload
-      if (Json.isInfoAcceptable(what)) {
-        uploadableWhat = (new Json(what) as unknown) as UploadableEntity<T, R>;
-      } else {
-        // There's nothing we can do
-        throw new Error("Can only upload UploadableFile-s or Json-file acceptable content.");
-      }
-    } else {
-      // upload what was given as is since it's an UploadableEntity type already
-      uploadableWhat = (what as unknown) as UploadableEntity<T, R>;
-    }
-    return uploadableWhat.uploadTo({ session: this, args });
   }
 
   /**
@@ -166,45 +160,51 @@ export class ApiSession implements SolidityAddressable {
     });
   }
 
-  /**
-   * Executes a {@link TransactionReceiptQuery} with the purpose of optaining a {@link TransactionReceipt} for apreviously executed, network, operation.
+    /**
+   * Given an {@link UploadableEntity}, it triest ot upload it using the currently configured {@link ApiSession} passing in-it any provided {@link args}.
+   * 
+   * @param {Uploadable} what - The {@link UploadableEntity} to push through this {@link ApiSession}
+   * @param {*} args - A list of arguments to pass through the upload operation itself.
+   *                   Note: this list has, by convention, at various unpaking stages in the call hierarchy, the capabilities to specify SDK behaviour through
+   *                         eg. "_file" ({@link UploadableEntity}) or "_contract" ({@link Contract})
+   * @returns - An instance of the {@link UploadableEntity} concrete result-type which is a subtype of {@link LiveEntity}.
    */
-  public async execute(transaction: TransactionReceiptQuery): Promise<TransactionReceipt>;
-  /**
-   * Queries/Executes a contract function, returning the {@link ContractFunctionResult} if successfull.
-   */
-  public async execute(transaction: ContractFunctionCall): Promise<ContractFunctionResult>;
-  /**
-   * A catch-all/generic {@link Transaction} execution operation yeilding, upon success, of a {@link TransactionResponse}.
-   */
-  public async execute(transaction: Transaction): Promise<TransactionResponse>;
-  public async execute(transaction: ContractFunctionCall|TransactionReceiptQuery|Transaction): Promise<ContractFunctionResult|TransactionReceipt|TransactionResponse> {
-    const isContractTransaction = transaction instanceof ContractCallQuery || transaction instanceof ContractExecuteTransaction;
-    const isReceiptQuery = transaction instanceof TransactionReceiptQuery;
-    const txResponse = await transaction.execute(this.client);
+     public async upload<T extends LiveEntity<R>, R>(what: UploadableEntity<T, R>, ...args: any[]): Promise<T>;
 
-    if (isContractTransaction) {
-      if (txResponse instanceof TransactionResponse) {
-        const txRecord = await txResponse.getRecord(this.client);
-  
-        // Inspired from https://github.com/hashgraph/hedera-sdk-js/blob/c4a8d339648651a545782089ae4b18b972f2e356/src/transaction/TransactionResponse.js#L39
-        // since, at this step, getRecord errors are the same as getReceipt ones
-        if (txRecord.receipt.status !== Status.Success) {
-          throw new ReceiptStatusError({
-              transactionReceipt: txRecord.receipt,
-              status: txRecord.receipt.status,
-              transactionId: txResponse.transactionId,
-          });
-        }
-        return txRecord.contractFunctionResult;
-      } else {
-        // Constant function call (query) was done
-        return txResponse as ContractFunctionResult;
-      }
-    } else if (isReceiptQuery) {
-      return (txResponse as unknown) as TransactionReceipt;
-    }
-    // Reaching this point means that the executed transaction is generic in which case we just return it rawly
-    return txResponse as TransactionResponse;
-  }
+     /**
+      * Given a raw JSON {@link object}, it triest ot upload it using the currently configured {@link Client} passing in-it any provided {@link args}.
+      * 
+      * `Note:` This is the same as calling the more verbose equivalent of `upload(new Json(what))`.
+      * 
+      * Example of usage:
+      * ```js
+      * await apiSession.upload({a: 1, {b: "c"}})
+      * ```
+      * 
+      * @param {object} what - The {@link Json}-acceptable payload to push through this {@link ApiSession}
+      * @param {*} args - A list of arguments to pass through the upload operation itself.
+      *                   Note: this list has, by convention, at various unpaking stages in the call hierarchy, the capabilities to specify SDK behaviour through
+      *                         eg. "_file" ({@link Uploadable}) or "_contract" ({@link Contract})
+      * @returns - An instance of the associated {@link LiveJson} resulting {@link LiveEntity}.
+      */
+     public async upload(what: object, ...args: any[]): Promise<LiveJson>;
+   
+     // Overload implementation
+     public async upload<T extends LiveEntity<R>, R>(what: UploadableEntity<T, R>|object, ...args: any[]): Promise<T|LiveJson> {
+       let uploadableWhat: UploadableEntity<T, R>;
+   
+       if (what instanceof UploadableEntity === false) {
+         // Try to go with a live-json upload
+         if (Json.isInfoAcceptable(what)) {
+           uploadableWhat = (new Json(what) as unknown) as UploadableEntity<T, R>;
+         } else {
+           // There's nothing we can do
+           throw new Error("Can only upload UploadableFile-s or Json-file acceptable content.");
+         }
+       } else {
+         // upload what was given as is since it's an UploadableEntity type already
+         uploadableWhat = (what as unknown) as UploadableEntity<T, R>;
+       }
+       return uploadableWhat.uploadTo({ session: this, args });
+     }
 }
