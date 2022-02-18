8624cfc86b9215e0dec3507fbb9f33062eecf238
Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
2022-02-18 12:36:28 +0200

    Fixed re-entrancy issue with SolidiyCompiler - the web-worker version.

--- docs/src/strato/polyfills/SolidityCompiler.js
+++ docs/src/strato/polyfills/SolidityCompiler.js
@@ -6,6 +6,34 @@ export const VIRTUAL_SOURCE_CONTRACT_FILE_NAME = '__contract__.sol';
 let compilerWorker = null;
 let hasCompilerLoaded = false;
 
+function triggerCompile(code) {
+  const solInput = {
+    language: 'Solidity',
+    settings: {
+      metadata: {
+        // disabling metadata hash embedding to make the bytecode generation predictable at test-time
+        // see https://docs.soliditylang.org/en/latest/metadata.html#encoding-of-the-metadata-hash-in-the-bytecode
+        bytecodeHash: process.env.NODE_ENV === 'test' ? 'none' : 'ipfs'
+      },
+      outputSelection: {
+        '*': {
+          '*': ['*']
+        }
+      }
+    },
+    sources: { [VIRTUAL_SOURCE_CONTRACT_FILE_NAME]: { content: code }},
+  };
+
+  if (!compilerWorker) {
+    throw new Error("First initialize the compile web-worker.");
+  }
+
+  compilerWorker.postMessage({ 
+    payload: JSON.stringify(solInput),
+    type: 'compile'
+  });
+}
+
 export class SolidityCompiler {
   static compile({ code, path }) {
     if (!window.Worker) {
@@ -16,43 +44,25 @@ export class SolidityCompiler {
     }
     if (!compilerWorker) {
       compilerWorker = new CompilerWorker();
+
+      return new Promise((accept, reject) => {
+        compilerWorker.onmessage = function({ data }) {
+          if (data.type === 'compile_result') {
+            accept(data.payload);
+          } else if (data.type === 'loaded' || hasCompilerLoaded) {
+            hasCompilerLoaded = true;
+            triggerCompile(code);
+            return;
+          } else {
+            console.log("Unhandled message received from web-worker:", data);
+          }
+        };
+        compilerWorker.onerror = function(e) {
+          reject(e);
+        };
+      });
     }
 
-    const solInput = {
-      language: 'Solidity',
-      settings: {
-        metadata: {
-          // disabling metadata hash embedding to make the bytecode generation predictable at test-time
-          // see https://docs.soliditylang.org/en/latest/metadata.html#encoding-of-the-metadata-hash-in-the-bytecode
-          bytecodeHash: process.env.NODE_ENV === 'test' ? 'none' : 'ipfs'
-        },
-        outputSelection: {
-          '*': {
-            '*': ['*']
-          }
-        }
-      },
-      sources: { [VIRTUAL_SOURCE_CONTRACT_FILE_NAME]: { content: code }},
-    };
-
-    return new Promise((accept, reject) => {
-      compilerWorker.onmessage = function({ data }) {
-        if (data.type === 'compile_result') {
-          accept(data.payload);
-        } else if (data.type === 'loaded' || hasCompilerLoaded) {
-          hasCompilerLoaded = true;
-          compilerWorker.postMessage({ 
-            payload: JSON.stringify(solInput),
-            type: 'compile'
-          });
-          return;
-        } else {
-          console.log("Unhandled message received from web-worker:", data);
-        }
-      };
-      compilerWorker.onerror = function(e) {
-        reject(e);
-      };
-    });
+    triggerCompile(code);
   }
 }
