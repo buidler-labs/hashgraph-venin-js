61223ece5423cbea09d7d6d686a106e622927fe2
Victor ADASCALITEI <210908+3Nigma@users.noreply.github.com>
2022-02-25 17:01:00 +0200

    First attempt to fix the test:jsdom npm script. Due to Jest #1612, it looks like it's missing the window.crypto feature.

---
+++ test/setup.web.ts
@@ -0,0 +1,5 @@
+import { Crypto } from "@peculiar/webcrypto";
+
+// Resolves https://github.com/jsdom/jsdom/issues/1612
+//    which affects ECSDA key generation in the elliptic library
+window.crypto = new Crypto();
