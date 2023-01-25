2abf7706d58f1c41b96f0c3365d4e414e3cd4941
Victor Holotescu <victor.holotescu@yahoo.ro>
2023-01-25 14:55:48 +0200

    Update peerDep hedera sdk to 2.19.2 and bump and publish beta.4

--- test/solidity-by-example/specs/LiveContract.Solidity-by-Example.meta-args.spec.ts
+++ test/solidity-by-example/specs/LiveContract.Solidity-by-Example.meta-args.spec.ts
@@ -45,7 +45,7 @@ describe("LiveContract.Solidity-by-Example.meta-arguments", () => {
       metaArgs.paymentTransactionId
     );
     // Expect actual query-payment to be capped by the max-query-payment since query-payment > max-query-payment
-    expect(queryTransaction._queryPayment).toEqual(metaArgs.maxQueryPayment);
+    expect(queryTransaction._queryPayment).toEqual(metaArgs.queryPayment);
   });
 
   it("when calling a state-changing contract method, passing in meta-arguments should get propagated to the actual transaction", async () => {
